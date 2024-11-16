c74697776ec0314d6f7ef13f4d8e4621ded68e36
Colby Snyder <ctsnyder12@gmail.com>
2024-11-16 12:26:59 -0600

    nvim files

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local Snacks = require("snacks")
+local copilot_exists = pcall(require, "copilot")
+
+if copilot_exists then
+  Snacks.toggle({
+    name = "Copilot Completion",
+    color = {
+      enabled = "azure",
+      disabled = "orange"
+    },
+    get = function()
+      return not require("copilot.client").is_disabled()
+    end,
+    set = function(state)
+      if state then
+        require("copilot.command").enable()
+      else
+        require("copilot.command").disable()
+      end
+    end,
+  }):map("<leader>at")
+end
